70b38c29e494c3ca146425ba4ff96b0924b7ac39
tianfanshan <stf30111994@gmail.com>
2022-06-20 13:36:05 +0200

    primer commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,24 @@
 import { BrowserRouter,Routes, Route } from 'react-router-dom';
 import './App.css';
+
 import Products from './components/Products/Products';
+import Login from './components/Login/Login'
+
+import { UserProvider } from './context/UserContext/UserState';
 import { GlobalProvider } from './context/GlobalState';
+import 'antd/dist/antd.css'
 
 function App() {
   return (
     <div className="App">
      <BrowserRouter>
         <GlobalProvider>
-          <Routes>
+        <UserProvider>
+        <Routes>
             <Route path="/products" element={<Products />} />
+            <Route path='/' element={<Login/>}/>
           </Routes>
+        </UserProvider>
         </GlobalProvider>
       </BrowserRouter>
     </div>
---
+++ src/components/Login/Login.jsx
@@ -0,0 +1,68 @@
+import { useContext,useEffect } from 'react'
+import { UserContext } from '../../context/UserContext/UserState'
+import { Form, Input, Button} from 'antd'
+import { useNavigate} from 'react-router-dom'
+
+const Login = () => {
+
+    const { login } = useContext(UserContext)
+
+    const navigate = useNavigate()
+
+    const onFinish = (values) =>{
+        console.log(values)
+        login(values)
+    }
+
+    const onFinishFailed = (errorInfo) => {
+        console.log('Failed:',errorInfo)
+    }
+
+    useEffect(()=>{
+         setTimeout(()=>{
+            const foundToken = JSON.parse(localStorage.getItem('token'))
+            if(foundToken){
+                console.log('hay token')
+                // navigate()
+            }
+         },2000)
+    },[login])
+
+  return (
+    <div className="container">
+          <Form
+            name="basic"
+            labelCol={{ span: 8 }}
+            wrapperCol={{ span: 16 }}
+            initialValues={{ remember: true }}
+            onFinish={onFinish}
+            onFinishFailed={onFinishFailed}
+            autoComplete="off"
+          >
+            <Form.Item
+              label="Email"
+              name="email"
+              rules={[{ required: true, message: "Please input your email!" }]}
+            >
+              <Input />
+            </Form.Item>
+    
+            <Form.Item
+              label="Password"
+              name="password"
+              rules={[{ required: true, message: "Please input your password!" }]}
+            >
+              <Input.Password />
+            </Form.Item>
+    
+            <Form.Item wrapperCol={{ offset: 8, span: 16 }}>
+              <Button type="primary" htmlType="submit">
+                    Submit
+              </Button>
+            </Form.Item>
+          </Form>
+        </div>
+  )
+}
+
+export default Login
