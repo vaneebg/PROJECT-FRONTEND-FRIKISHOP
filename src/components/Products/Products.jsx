8c28efd6e4b4033d97324726df2300a6b4311cc0
tianfanshan <stf30111994@gmail.com>
2022-06-21 13:38:07 +0200

    orderscontext eliminado

--- src/components/Products/Products.jsx
+++ src/components/Products/Products.jsx
@@ -2,27 +2,18 @@ import { useContext, useEffect } from "react";
 import { ProductsContext } from "../../context/ProductsContext/ProductsState";
 
 const Products = () => {
-  const { products, getProducts,addCart, cart} = useContext(ProductsContext);
+  const { products, getProducts, addCart} = useContext(ProductsContext);
+  
   useEffect(() => {
     getProducts();
   }, []);
-
-  useEffect(() => {
-    localStorage.setItem("cart", JSON.stringify(cart));
-  }, [cart]);
 
     if (products.length <= 0) {
     return <span>Cargando...</span>;
   }
   
-  const listProducts=products.map(product=>{return(<>
-  <h2>{product.name}</h2> 
-  <span>Descripción:{product.description}</span><br/>
-  <span>Precio:{product.price}</span><br/>
-  <span>Stock:{product.stock}</span>
-  <img src={"http://localhost:8080/images/products/"+product.img}/>
-  <button onClick={() => addCart(product)}>Añadir al carrito</button>
-  </>)})
+  const listProducts=products.map(product=>{return(<><h2>{product.name}</h2> <span>Descripción:{product.description}</span><br/><span>Precio:{product.price}</span><br/><span>Stock:{product.stock}</span>
+  <img src={"http://localhost:8080/images/products/"+product.img}/></>)})
 
 
     return (
