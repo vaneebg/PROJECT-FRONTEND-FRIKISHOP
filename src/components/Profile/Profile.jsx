2e08c072e77e54a11c45ee2ae44883e64c3cba0e
Vanesa Beltrán Guillén <vaneebg@gmail.com>
2022-06-21 20:20:25 +0200

    hecho estilo perfil

--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -1,3 +1,4 @@
+import './Profile.scss'
 import React from 'react'
 import { useContext, useEffect } from 'react'
 import { UserContext } from '../../context/UserContext/UserState'
@@ -14,15 +15,22 @@ const Profile = () => {
     const listOrders=user.Orders.map(order=>order.Products.map(product=>{return(<>
     <span>Nombre del producto: {product.name}</span><br/>
     <span>Descripción: {product.description}</span><br/></>)}))
-console.log("aquii",user.img)
   return (
-    <div>
+    <div className="center">
+    <div className='profile'>
         <h1>Perfil</h1>  
+        <div className="contentInfo">
         <img src={"http://localhost:8080/images/users/"+user.img}/>
+        <div className="text">
         <span>Tu nombre: {user.username}</span> <br />
         <span>Tu email: {user.email}</span><br />
         <span>Tu cueva: {user.adress}</span><br/>
-        <span>Pedidos: <br/>{listOrders}</span>
+        </div>
+        </div>
+        <div className="orders">
+        <span><div className="titleorder">Pedidos hechos:</div> <br/>{listOrders}</span>
+        </div>
+        </div>
         </div>
   )
 }
