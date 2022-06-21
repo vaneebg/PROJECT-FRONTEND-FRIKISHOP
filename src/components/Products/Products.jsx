17635d6884d1bd54038523062bb225c9a1b44417
Vanesa Beltrán Guillén <vaneebg@gmail.com>
2022-06-21 14:04:27 +0200

    boton bug product solucionado

--- src/components/Products/Products.jsx
+++ src/components/Products/Products.jsx
@@ -12,8 +12,13 @@ const Products = () => {
     return <span>Cargando...</span>;
   }
   
-  const listProducts=products.map(product=>{return(<><h2>{product.name}</h2> <span>Descripción:{product.description}</span><br/><span>Precio:{product.price}</span><br/><span>Stock:{product.stock}</span>
-  <img src={"http://localhost:8080/images/products/"+product.img}/></>)})
+  const listProducts=products.map(product=>{return(<>
+  <h2>{product.name}</h2> 
+  <span>Descripción:{product.description}</span><br/>
+  <span>Precio:{product.price}</span><br/>
+  <span>Stock:{product.stock}</span>
+  <img src={"http://localhost:8080/images/products/"+product.img}/>
+  <button onClick={() => addCart(product)}>Add Cart</button></>)})
 
 
     return (
