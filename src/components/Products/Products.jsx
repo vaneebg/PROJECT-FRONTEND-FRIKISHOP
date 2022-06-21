d133d1b6829640697ad27b3ac8b8e84276a36cd7
Vanesa Beltrán Guillén <vaneebg@gmail.com>
2022-06-21 12:22:02 +0200

    modificado multer products

--- src/components/Products/Products.jsx
+++ src/components/Products/Products.jsx
@@ -12,7 +12,7 @@ const Products = () => {
   }
   
   const listProducts=products.map(product=>{return(<><h2>{product.name}</h2> <span>Descripción:{product.description}</span><br/><span>Precio:{product.price}</span><br/><span>Stock:{product.stock}</span>
-  <img src={"http://localhost:8080/"+product.img}/></>)})
+  <img src={"http://localhost:8080/images/products/"+product.img}/></>)})
 
 
   return (
