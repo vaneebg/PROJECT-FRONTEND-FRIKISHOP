6aa23db245f53ca9050164c753b3f6c83afffbde
Vanesa Beltrán Guillén <vaneebg@gmail.com>
2022-06-21 16:20:15 +0200

    merge fixed

--- src/components/Products/Products.jsx
+++ src/components/Products/Products.jsx
@@ -16,23 +16,12 @@ const Products = () => {
   
   const listProducts=products.map(product=>{return(<div className='product'>
   <h2>{product.name}</h2> 
-<<<<<<< HEAD
   <div className='content'>
   <span>Descripción: {product.description}<br/>
   Precio: {product.price}€<br/>
   Stock: {product.stock}</span><br/>
   <img src={"http://localhost:8080/images/products/"+product.img}/></div>
   <button onClick={() => addCart(product)}>Añadir a carrito</button></div>)})
-=======
-  <span>Descripción:{product.description}</span><br/>
-  <span>Precio:{product.price}</span><br/>
-  <span>Stock:{product.stock}</span>
-  <img src={"http://localhost:8080/images/products/"+product.img}/>
-  {token?<button onClick={() => addCart(product)}>Añadir a carrito</button>:null}
-  </>
-  )
-  })
->>>>>>> 889fdc09ec5fb5225b09e8979539b7ef48ff27c0
 
 
     return (
