05f4fd23bc16f1da675c16b8fd705ed3408d138f
Vanesa Beltrán Guillén <vaneebg@gmail.com>
2022-06-21 20:01:44 +0200

    añadida img en perfil del usuario

--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -14,10 +14,11 @@ const Profile = () => {
     const listOrders=user.Orders.map(order=>order.Products.map(product=>{return(<>
     <span>Nombre del producto: {product.name}</span><br/>
     <span>Descripción: {product.description}</span><br/></>)}))
-
+console.log("aquii",user.img)
   return (
     <div>
         <h1>Perfil</h1>  
+        <img src={"http://localhost:8080/images/users/"+user.img}/>
         <span>Tu nombre: {user.username}</span> <br />
         <span>Tu email: {user.email}</span><br />
         <span>Tu cueva: {user.adress}</span><br/>
