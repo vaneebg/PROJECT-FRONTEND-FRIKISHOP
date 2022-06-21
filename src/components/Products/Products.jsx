39f6f595d68463989d62df9d8f186964c560617b
Vanesa Beltrán Guillén <vaneebg@gmail.com>
2022-06-21 16:19:10 +0200

    estilos iniciales productos

--- src/components/Products/Products.jsx
+++ src/components/Products/Products.jsx
@@ -1,6 +1,6 @@
 import { useContext, useEffect } from "react";
 import { ProductsContext } from "../../context/ProductsContext/ProductsState";
-
+import './Products.scss'
 const Products = () => {
   const { products, getProducts, addCart} = useContext(ProductsContext);
   
@@ -12,21 +12,24 @@ const Products = () => {
     return <span>Cargando...</span>;
   }
   
-  const listProducts=products.map(product=>{return(<>
+  const listProducts=products.map(product=>{return(<div className='product'>
   <h2>{product.name}</h2> 
-  <span>Descripción:{product.description}</span><br/>
-  <span>Precio:{product.price}</span><br/>
-  <span>Stock:{product.stock}</span>
-  <img src={"http://localhost:8080/images/products/"+product.img}/>
-  <button onClick={() => addCart(product)}>Añadir a carrito</button></>)})
+  <div className='content'>
+  <span>Descripción: {product.description}<br/>
+  Precio: {product.price}€<br/>
+  Stock: {product.stock}</span><br/>
+  <img src={"http://localhost:8080/images/products/"+product.img}/></div>
+  <button onClick={() => addCart(product)}>Añadir a carrito</button></div>)})
 
 
     return (
-      <div>
+    <>
         <h1> Productos</h1>
+        <div className='containerProducts'>
         {listProducts}
         {products.name} 
       </div>
+      </>
     );
 };
 
