f092d0fe79b4f80c52efe5c1605fd6181b75b382
Vanesa Beltrán Guillén <vaneebg@gmail.com>
2022-06-22 21:33:01 +0200

    bugs fixed

--- src/components/Products/AddProduct/AddProduct.jsx
+++ src/components/Products/AddProduct/AddProduct.jsx
@@ -1,4 +1,4 @@
-import { useState, useContext } from "react";
+import {  useContext } from "react";
 import { ProductsContext } from "../../../context/ProductsContext/ProductsState";
 import './AddProduct.scss'
 import { Form, Input, Button, InputNumber, notification } from 'antd';
@@ -7,7 +7,6 @@ import { Form, Input, Button, InputNumber, notification } from 'antd';
 const AddProduct = () => {
 
   const { addProduct } = useContext(ProductsContext);
-
  
   const onFinish = (values) => {  
     addProduct(values)
@@ -20,49 +19,24 @@ const AddProduct = () => {
  
   const onFinishFailed = (errorInfo) => {
     console.log("Failed:", errorInfo);
+    return notification.error({
+      message: "Meec",
+      description: "No ha ido bien...",
+    });
   };
   return (
     <div className="container">
-   <Form
-            name="basic"
-            labelCol={{ span: 8 }}
-            wrapperCol={{ span: 16 }}
-            initialValues={{ remember: true }}
+   <Form name="basic" labelCol={{ span: 8 }} wrapperCol={{ span: 16 }} initialValues={{ remember: true }}
             onFinish={onFinish}
             onFinishFailed={onFinishFailed}
-            autoComplete="off"
-          >
-             <Form.Item
-              label="Name"
-              name="name"
-            >
-              <Input />
-            </Form.Item>
-            <Form.Item
-              label="Description"
-              name="description"
-            >
-              <Input />
-            </Form.Item>
-            <Form.Item
-              label="Price"
-              name="price"
-            defaultValue={3}
-            >
-              <InputNumber />
-            </Form.Item>
-            <Form.Item
-              label="Stock"
-              name="stock"
-            >
-              <InputNumber />
-            </Form.Item>
+            autoComplete="off" >
+            <Form.Item label="Name" name="name" > <Input /> </Form.Item>
+            <Form.Item label="Description"  name="description" > <Input /> </Form.Item>
+            <Form.Item label="Price" name="price" defaultValue={3}> <InputNumber /> </Form.Item>
+            <Form.Item label="Stock" name="stock"> <InputNumber /> </Form.Item>
             <Form.Item wrapperCol={{ offset: 8, span: 16 }}>
-              <Button type="primary" htmlType="submit">
-                    Crear producto
-              </Button>
-            </Form.Item>
-          </Form>
+             <Button type="primary" htmlType="submit"> Crear producto </Button> </Form.Item>
+     </Form>
       
     </div>
   );
