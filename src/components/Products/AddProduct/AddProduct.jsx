87267c3a538222d3a7de5a7b0aa5d07a362260c5
Vanesa Beltrán Guillén <vaneebg@gmail.com>
2022-06-22 21:38:13 +0200

    quitado spanglish

--- src/components/Products/AddProduct/AddProduct.jsx
+++ src/components/Products/AddProduct/AddProduct.jsx
@@ -26,13 +26,14 @@ const AddProduct = () => {
   };
   return (
     <div className="container">
+      <h2>Crear producto</h2>
    <Form name="basic" labelCol={{ span: 8 }} wrapperCol={{ span: 16 }} initialValues={{ remember: true }}
             onFinish={onFinish}
             onFinishFailed={onFinishFailed}
             autoComplete="off" >
-            <Form.Item label="Name" name="name" > <Input /> </Form.Item>
-            <Form.Item label="Description"  name="description" > <Input /> </Form.Item>
-            <Form.Item label="Price" name="price" defaultValue={3}> <InputNumber /> </Form.Item>
+            <Form.Item label="Nombre producto" name="name" > <Input /> </Form.Item>
+            <Form.Item label="Descripción"  name="description" > <Input /> </Form.Item>
+            <Form.Item label="Precio" name="price" defaultValue={3}> <InputNumber /> </Form.Item>
             <Form.Item label="Stock" name="stock"> <InputNumber /> </Form.Item>
             <Form.Item wrapperCol={{ offset: 8, span: 16 }}>
              <Button type="primary" htmlType="submit"> Crear producto </Button> </Form.Item>
