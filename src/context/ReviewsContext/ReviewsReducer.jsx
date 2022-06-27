6b64431db7aba5c499fb930c18d507dc64fff43b
Vanesa Beltrán Guillén <vaneebg@gmail.com>
2022-06-27 11:39:11 +0200

    borrada funcion no uso

--- src/context/ReviewsContext/ReviewsReducer.jsx
+++ src/context/ReviewsContext/ReviewsReducer.jsx
@@ -1,10 +1,6 @@
 const reviews =(state,action)=>{
     switch (action.type) {
-        case 'GET_ALL_REVIEWS':
-            return{
-                ...state,
-                reviews:action.payload
-            }
+        
         case 'GET_REVIEW_BY_ID':
             return{
                 ...state,
--- src/context/ReviewsContext/ReviewsState.jsx
+++ src/context/ReviewsContext/ReviewsState.jsx
@@ -17,19 +17,7 @@ export const ReviewsProvider = ({children}) => {
 
     const [state,dispatch] = useReducer(ReviewsReducer,initialState)
 
-    const getReviews = async() =>{
-        const token = JSON.parse(localStorage.getItem('token'))
-            const res = await axios.get(API_URL + '/reviews/review_product',{
-                headers:{
-                    authorization:token,
-                }
-            })
-            console.log("aquii",res.data)
-            dispatch({
-                type:'GET_ALL_REVIEWS',
-                payload:res.data
-            })
-    }
+ 
     const getReview = async(produ)=>{
         console.log('hola')
         console.log('soy produ',produ)
@@ -51,7 +39,6 @@ export const ReviewsProvider = ({children}) => {
   return (<ReviewsContext.Provider
     value={{
         reviews:state.reviews,
-        getReviews,
         getReview
     }}
     >
