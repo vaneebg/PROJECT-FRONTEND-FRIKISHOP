f690d4e1b973c4760aed8ac0973307b90fb2fe78
tianfanshan <stf30111994@gmail.com>
2022-06-28 15:08:40 +0200

    console.log out

--- src/components/Reviews/Reviews.jsx
+++ src/components/Reviews/Reviews.jsx
@@ -1,22 +1,11 @@
 import React, { useState } from 'react'
 import { useContext } from 'react'
-import { ProductsContext } from '../../context/ProductsContext/ProductsState'
 import { ReviewsContext } from '../../context/ReviewsContext/ReviewsState'
-import { UserContext } from '../../context/UserContext/UserState'
 
 const Reviews = () => {
 
-  const { products } = useContext(ProductsContext)
   const { createReview, reviews,id, modifyReview } = useContext(ReviewsContext)
-  const { user } = useContext(UserContext)
-
-  console.log(id)
- let productId
-  // console.log('products',products)
-
-
-
-  console.log('reviews', reviews)
+  // const { user } = useContext(UserContext)
 
   const [title, setTitle] = useState('')
   const [score, setScore] = useState(0)
@@ -31,7 +20,6 @@ const Reviews = () => {
   const handleInputchange4 = (event) => {
     setBody(event.target.value)
   }
-  console.log(title)
   const createRevie = (e) => {
     e.preventDefault()
     createReview({ProductId:id, title, score, body })
