40949a316808d8895ada622ed11d2185f374209d
tianfanshan <stf30111994@gmail.com>
2022-06-28 15:37:55 +0200

    modify reviews 1.0

--- src/components/Reviews/Reviews.jsx
+++ src/components/Reviews/Reviews.jsx
@@ -1,11 +1,19 @@
 import React, { useState } from 'react'
 import { useContext } from 'react'
+import { ProductsContext } from '../../context/ProductsContext/ProductsState'
 import { ReviewsContext } from '../../context/ReviewsContext/ReviewsState'
+import { UserContext } from '../../context/UserContext/UserState'
 
 const Reviews = () => {
 
-  const { createReview, reviews,id, modifyReview } = useContext(ReviewsContext)
-  // const { user } = useContext(UserContext)
+  const { products } = useContext(ProductsContext)
+  const { createReview, reviews, id, modifyReview } = useContext(ReviewsContext)
+  const { user } = useContext(UserContext)
+
+
+
+
+  console.log('reviews', reviews)
 
   const [title, setTitle] = useState('')
   const [score, setScore] = useState(0)
@@ -20,21 +28,47 @@ const Reviews = () => {
   const handleInputchange4 = (event) => {
     setBody(event.target.value)
   }
+  console.log(title)
   const createRevie = (e) => {
     e.preventDefault()
-    createReview({ProductId:id, title, score, body })
+    createReview({ ProductId: id, title, score, body })
   }
 
 
 
   const listProducts = reviews.map((el, i) => {
+    
+  // const [title1,setTitle1 ] = useState(el.title) 
+  // const [score1,setScore1 ] = useState(el.score) 
+  // const [body1,setBody1 ] = useState(el.body) 
     return (
       <div>
-        <div key={i}>
-          <span>Título:{el.title}</span><br />
-          <span>Puntuacíon:{el.score}</span><br />
-          <span>Review:{el.body}</span>
-        </div>
+        {/* {user.id == el.UserId ?
+          <div>
+            <div key={i}>
+              <span>Título:{el.title}</span><br />
+              <span>Puntuacíon:{el.score}</span><br />
+              <span>Review:{el.body}</span>
+            </div>
+            <form action="" >
+              <span>Título review:</span><br />
+              <input type="text" onChange={setTitle1} value={title1}/><br />
+              <span>Puntuación:</span> <br />
+              <input type="number" max={"10"} min={"0"} onChange={setScore1} value={score1}/><br />
+              <span>Review:</span><br />
+              <input type="text" onChange={setBody1} value={body1}/><br />
+              <input type="submit" onClick={modifyReview} />
+            </form>
+          </div>
+
+          : */}
+          <div key={i}>
+            <span>Título:{el.title}</span><br />
+            <span>Puntuacíon:{el.score}</span><br />
+            <span>Review:{el.body}</span>
+          </div>
+
+        {/* } */}
       </div>
     )
   })
@@ -52,7 +86,7 @@ const Reviews = () => {
           <input type="number" max={"10"} min={"0"} onChange={handleInputchange3} /><br />
           <span>Review:</span><br />
           <input type="text" onChange={handleInputchange4} /><br />
-          <button onClick={createRevie} />
+          <input type="submit" onClick={createRevie} />
         </form>
       </div>
     </div>
